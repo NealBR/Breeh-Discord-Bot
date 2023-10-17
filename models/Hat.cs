205f6a492a7f709f284abb1c140c125835340d3e
Merlinovitsch <merlinovitsch@gmail.com>
2023-10-17 22:18:20 +0200

    Add more default values for Hat fields.

--- models/Hat.cs
+++ models/Hat.cs
@@ -10,11 +10,11 @@ namespace CSharp_Discord_Bot.models
     {
         public string caption { get; set; }
         public string filename { get; set; }
-        public bool enabled { get; set; }
-        public int monthFrom { get; set; }
-        public int dayFrom { get; set; }
-        public int monthTo { get; set; }
-        public int dayTo { get; set; }
+        public bool enabled { get; set; } = true;
+        public int monthFrom { get; set; } = 1;
+        public int dayFrom { get; set; } = 1;
+        public int monthTo { get; set; } = 12;
+        public int dayTo { get; set; } = 31;
 
         /// <summary>
         ///     Weight that determines the chance of this hat getting chosen.
